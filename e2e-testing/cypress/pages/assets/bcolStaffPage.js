cac492526068e5bc69c9705a0c0aee46af25bef4
DIVYA CHANDUPATLA <divya.chandupatla@gov.bc.ca>
2021-12-15 14:05:02 -0800

    updates to regression

---
+++ e2e-testing/cypress/pages/assets/bcolStaffPage.js
@@ -0,0 +1,35 @@
+/// <reference types="Cypress" />
+
+
+const log = require("log");
+const tab = require('cypress-plugin-tab')
+
+export class BCOLStaffPage {
+
+    constructor() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+
+
+
+
+
+
+
+
+
+}
+export const bcolStaffPage = new BCOLStaffPage()
